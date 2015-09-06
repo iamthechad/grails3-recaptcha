b0db725c33ae85c3933127238f92e42c2fdec99a
Chad Johnston <cjohnston@megatome.com>
2015-09-06 15:35:33 -0600

    Update plugin descriptor

--- src/main/groovy/com/megatome/grails/RecaptchaGrailsPlugin.groovy
+++ src/main/groovy/com/megatome/grails/RecaptchaGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 package com.megatome.grails
 
-import grails.plugins.*
+import grails.plugins.Plugin
 
 class RecaptchaGrailsPlugin extends Plugin {
 
@@ -11,60 +11,23 @@ class RecaptchaGrailsPlugin extends Plugin {
         "grails-app/views/error.gsp"
     ]
 
-    // TODO Fill in these fields
-    def title = "Grails Recaptcha" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def title = "ReCaptcha support for Grails" // Headline display name of the plugin
+    def author = "Chad Johnston"
+    def authorEmail = "cjohnston@megatome.com"
     def description = '''\
-Brief summary/description of the plugin.
+Protect your website from spam and abuse while letting real people pass through with ease.
 '''
     def profiles = ['web']
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/grails-recaptcha"
-
-    // Extra (optional) plugin metadata
+    def documentation = "http://iamthechad.github.com/grails-recaptcha/"
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def license = "APACHE"
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "Github", url: "https://github.com/iamthechad/grails-recaptcha/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-    Closure doWithSpring() { {->
-            // TODO Implement runtime spring config (optional)
-        } 
-    }
-
-    void doWithDynamicMethods() {
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    void doWithApplicationContext() {
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    void onChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    void onConfigChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    void onShutdown(Map<String, Object> event) {
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
+    def scm = [ url: "https://github.com/iamthechad/grails-recaptcha" ]
 }
