b6143e39354da1587fb699a899a8c5f96c1d5f2d
Chad Johnston <cjohnston@megatome.com>
2015-09-07 10:18:00 -0600

    Use correct URLs

--- src/main/groovy/com/megatome/grails/RecaptchaGrailsPlugin.groovy
+++ src/main/groovy/com/megatome/grails/RecaptchaGrailsPlugin.groovy
@@ -20,14 +20,14 @@ Protect your website from spam and abuse while letting real people pass through 
     def profiles = ['web']
 
     // URL to the plugin's documentation
-    def documentation = "http://iamthechad.github.com/grails-recaptcha/"
+    def documentation = "http://iamthechad.github.com/grails3-recaptcha/"
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
     def license = "APACHE"
 
     // Location of the plugin's issue tracker.
-    def issueManagement = [ system: "Github", url: "https://github.com/iamthechad/grails-recaptcha/issues" ]
+    def issueManagement = [ system: "Github", url: "https://github.com/iamthechad/grails3-recaptcha/issues" ]
 
     // Online location of the plugin's browseable source code.
-    def scm = [ url: "https://github.com/iamthechad/grails-recaptcha" ]
+    def scm = [ url: "https://github.com/iamthechad/grails3-recaptcha" ]
 }
